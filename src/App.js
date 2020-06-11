f4da9dfe9f5947940f69ccd2215fa4d3c3d3c247
Peter Xuan' ang <peterxuanang@hotmail.com>
2020-06-11 06:00:16 -0500

    Starting with learning about props and state in class-based components.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/FPerson'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Jane', age: 21 },
+      { name: 'Nat', age: 20 }
+    ]
+  }
+
+  changeNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: 'Jane Kroger', age: 21 },
+        { name: 'Nathaniel Long', age: 22 }
+      ]
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Introduction</h1>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <button onClick={this.changeNameHandler}>Update Person</button>
+      </div>
+    );
+  }
 }
 
 export default App;
