bdd8e3685a34dfa054aa190112385df48c86a800
Peter Xuan' ang <peterxuanang@hotmail.com>
2020-06-11 06:41:23 -0500

    Using Function-based components to change state(s).

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/FPerson'
 
-class App extends Component {
-  state = {
+// Using the useState() Hooks for Function-based components
+
+const App = props => {
+  // useState returns an array which we destructured as state, setState
+  // It's similar to what we have in class-based comps too just that this keyword is absent
+  const [state, setState] = useState({
     persons: [
       { name: 'Jane', age: 21 },
       { name: 'Nat', age: 20 }
     ]
+  })
+
+  const changeNameHandler = () => {
+    setState({
+      persons: [
+        { name: 'Jane Kroger', age: 21 },
+        { name: 'Nathaniel Long', age: 22 }
+      ]
+    })
   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Introduction</h1>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>Introduction</h1>
+      <button onClick={changeNameHandler}>Update Names</button>
+      <Person name={state.persons[0].name} age={state.persons[0].age} />
+      <Person name={state.persons[1].name} age={state.persons[1].age} />
+    </div>
+  );
 }
 
 export default App;
