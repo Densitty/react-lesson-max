c07704d26a39652192c36de15cb189e4d632dc86
Peter Xuan' ang <peterxuanang@hotmail.com>
2020-06-11 14:36:16 -0500

    My first task. Worked with an event and an event-handler using class-based components.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,35 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/FPerson'
+import Input from './Person/UserInput'
+import Output from './Person/UserOutput'
 
-// Using the useState() Hooks for Function-based components
-
-const App = props => {
-  // useState returns an array which we destructured as state, setState
-  // It's similar to what we have in class-based comps too just that this keyword is absent
-  // useState() hooks can be used many times, most especially if there is a state that we don't want to change but still want to be displayed
-  const [state, setState] = useState({
-    persons: [
-      { name: 'Jane', age: 21 },
-      { name: 'Nat', age: 20 }
-    ]
-  })
-
-  const [otherState, setotherState] = useState({ name: 'Edgar Davies' })
-  console.log(state, otherState)
-
-  const changeNameHandler = () => {
-    setState({
-      persons: [
-        { name: 'Jane Kroger', age: 21 },
-        { name: 'Nathaniel Long', age: 22 }
-      ]
-    })
-    setotherState({ name: 'Edgar Davies' })
+class App extends Component {
+  state = {
+    username: 'User'
   }
 
-  return (
-    <div className="App">
-      <h1>Introduction</h1>
-      <button onClick={changeNameHandler}>Update Names</button>
-      <Person name={state.persons[0].name} age={state.persons[0].age} />
-      <Person name={state.persons[1].name} age={state.persons[1].age} />
-    </div>
-  );
+  changeUsernameHandler = (event) => {
+    // console.log(event.target.value)
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <Input
+          change={this.changeUsernameHandler}
+          placeholder='Username'
+        />
+        <Output
+          userName={this.state.username}
+        />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/Person/UserOutput.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Output = props => {
+  return (
+    <div>
+      <p>Hello everyone. My name is {props.userName}.</p>
+    </div>
+  )
+}
+
+export default Output
