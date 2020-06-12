838bfd6e137ede257cbf469c652eae058d448051
Peter Xuan' ang <peterxuanang@hotmail.com>
2020-06-12 01:16:17 -0500

    Rendering lists (output of an array) with conditionals.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,61 @@
 import React, { Component } from 'react';
 import './App.css';
-import Input from './Person/UserInput'
-import Output from './Person/UserOutput'
+import Person from './Person/Person'
 
 class App extends Component {
   state = {
-    username: 'User'
+    persons: [
+      { name: 'Jane', age: 21 },
+      { name: 'Nat', age: 20 }
+    ],
+    showPersons: false
   }
 
-  changeUsernameHandler = (event) => {
-    // console.log(event.target.value)
+  changeNameHandler = () => {
+
     this.setState({
-      username: event.target.value
+      persons: [
+        { name: 'Jane Kroger', age: 21 },
+        { name: 'Nathaniel Long', age: 22 }
+      ]
     })
   }
 
+  showPersonHandler = () => {
+    const didPersonShow = this.state.showPersons
+    this.setState({
+      showPersons: !didPersonShow
+    })
+  }
 
+  // deletePersonHandler = () => {
+  //   const persons = this.state
+  // }
 
   render() {
+    let persons = null
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map(person => {
+            return <Person
+              name={person.name}
+              age={person.age}
+            />
+          })}
+        </div>
+      )
+    }
+
     return (
-      <div>
-        <Input
-          change={this.changeUsernameHandler}
-          placeholder='Username'
-        />
-        <Output
-          userName={this.state.username}
-        />
+      <div className="App">
+        <h1>Introduction</h1>
+        <button onClick={this.showPersonHandler}>Show Persons</button>
+        <button onClick={this.changeNameHandler}>Update Persons</button>
+        {persons}
       </div>
-    )
+    );
   }
 }
 
