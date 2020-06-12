c108f01992d76da4d35a7574a49d4c55e90bdafd
Peter Xuan' ang <peterxuanang@hotmail.com>
2020-06-12 07:22:06 -0500

    Made a final change to my list manipulation. Will come back to add more.

--- src/App.js
+++ src/App.js
@@ -1,71 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'
+import Input from './Person/UserInput'
+import Output from './Person/UserOutput'
 
 class App extends Component {
   state = {
-    persons: [
-      { id: '1', name: 'Jane', age: 21 },
-      { id: '2', name: 'Nat', age: 20 }
-    ],
-    showPersons: false
+    username: 'User'
   }
 
-  changeNameHandler = () => {
-
+  changeUsernameHandler = (event) => {
+    // console.log(event.target.value)
     this.setState({
-      persons: [
-        { id: '1', name: 'Jane Kroger', age: 21 },
-        { id: '2', name: 'Nathaniel Long', age: 22 }
-      ]
+      username: event.target.value
     })
   }
 
-  showPersonHandler = () => {
-    const didPersonShow = this.state.showPersons
-    this.setState({
-      showPersons: !didPersonShow
-    })
-  }
 
-  deletePersonHandler = (personIndex) => {
-    console.log(personIndex)
-    // The best practice is to create a copy of the this.state.persons object/array and mutate it instead of mutating the original persons object/array
-    // const updatedPersons = this.state.persons.slice() //or below
-    const updatedPersons = [...this.state.persons] //spread content of persons
-    console.log(updatedPersons)
-    updatedPersons.splice(personIndex, 1)
-    this.setState({ persons: updatedPersons })
-    console.log(this.state.persons)
-
-  }
 
   render() {
-    let persons = null
-
-    if (this.state.showPersons) {
-      persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <Person
-              click={() => this.deletePersonHandler(index)}
-              name={person.name}
-              age={person.age}
-              key={person.id}
-            />
-          })}
-        </div>
-      )
-    }
-
     return (
-      <div className="App">
-        <h1>Introduction</h1>
-        <button onClick={this.showPersonHandler}>Show Persons</button>
-        <button onClick={this.changeNameHandler}>Update Persons</button>
-        {persons}
+      <div>
+        <Input
+          change={this.changeUsernameHandler}
+          placeholder='Username'
+        />
+        <Output
+          userName={this.state.username}
+        />
       </div>
-    );
+    )
   }
 }
 
