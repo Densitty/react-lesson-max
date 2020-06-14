acaaf85a55dd8fe88bcae0f8a7f00244d0e2472a
Peter Xuan' ang <peterxuanang@hotmail.com>
2020-06-14 05:57:21 -0500

    Worked on Radium. Will continue later with CSS Modules.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium, { StyleRoot } from 'radium' // package for using pseudo-class
 import Person from './Person/Person'
 
 class App extends Component {
@@ -33,6 +34,18 @@ class App extends Component {
   // }
 
   render() {
+    // We can set the style inline
+    const style = {
+      backgroundColor: 'green',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+      ':hover': {//hover pseudo-selector
+        backgroundColor: 'lightgreen',
+        color: 'white'
+      }
+    }
+
     let persons = null
 
     if (this.state.showPersons) {
@@ -46,17 +59,26 @@ class App extends Component {
           })}
         </div>
       )
+      // to make our button change style on state change in an inline-way
+      style.backgroundColor = 'red'
+      style.color = 'white'
+      style[':hover'] = {
+        backgroundColor: 'lightred',
+        color: 'black'
+      }
     }
 
     return (
-      <div className="App">
-        <h1>Introduction</h1>
-        <button onClick={this.showPersonHandler}>Show Persons</button>
-        <button onClick={this.changeNameHandler}>Update Persons</button>
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Introduction</h1>
+          <button style={style} onClick={this.showPersonHandler}>Show Persons</button>
+          <button onClick={this.changeNameHandler}>Update Persons</button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
   }
 }
 
-export default App;
+export default Radium(App);
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import Radium, { StyleRoot } from 'radium'
 import './Person.css'
 
 const Person = (props) => {
+  // media queries using Radium
+  const style = {
+    '@media (min-width: 500px)': {
+      width: '450px'
+    }
+  }
   return (
-    <div className='Person'>
-      <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old!</p>
-      <p>{props.children}</p>
-    </div>
+    <StyleRoot>
+      <div className='Person'>
+        <p style={style} onClick={props.click}>I'm {props.name} and I'm {props.age} years old!</p>
+        <p>{props.children}</p>
+      </div>
+    </StyleRoot>
   )
 }
 
-export default Person
+export default Radium(Person)
