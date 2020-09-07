e79e226a9e44dce54f64abaf65d2fb8083b51fe8
Peter Xuan' ang <peterxuanang@hotmail.com>
2020-09-07 07:24:34 -0500

    HOC further explained.

--- src/App.js
+++ src/App.js
@@ -1,62 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person'
+import React from 'react'
 
-class App extends Component {
-  state = {
-    persons: [
-      { name: 'Jane', age: 21 },
-      { name: 'Nat', age: 20 }
-    ],
-    showPersons: false
-  }
+import User from './Components/User'
 
-  changeNameHandler = () => {
 
-    this.setState({
-      persons: [
-        { name: 'Jane Kroger', age: 21 },
-        { name: 'Nathaniel Long', age: 22 }
-      ]
-    })
-  }
-
-  showPersonHandler = () => {
-    const didPersonShow = this.state.showPersons
-    this.setState({
-      showPersons: !didPersonShow
-    })
-  }
-
-  // deletePersonHandler = () => {
-  //   const persons = this.state
-  // }
-
-  render() {
-    let persons = null
-
-    if (this.state.showPersons) {
-      persons = (
-        <div>
-          {this.state.persons.map(person => {
-            return <Person
-              name={person.name}
-              age={person.age}
-            />
-          })}
-        </div>
-      )
-    }
-
-    return (
-      <div className="App">
-        <h1>Introduction</h1>
-        <button onClick={this.showPersonHandler}>Show Persons</button>
-        <button onClick={this.changeNameHandler}>Update Persons</button>
-        {persons}
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div>
+      <User name='Celine' />
+    </div>
+  )
 }
 
-export default App;
+export default App
