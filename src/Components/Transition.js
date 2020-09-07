9ba6386aa92e43e3ed265a916bca3254946e6f00
Peter Xuan' ang <peterxuanang@hotmail.com>
2020-09-07 13:25:03 -0500

    React Transition using CSS classes instead of inline-styles.

--- src/Components/Transition.js
+++ src/Components/Transition.js
@@ -21,19 +21,25 @@ class TransitionComp extends Component {
       <div>
         <Transition
           in={this.state.show}
-          timeout={2000}
+          timeout={{
+            enter: 2000,
+            exit: 50
+          }}
 
-          mountOnEnter
-          unmountOnExit
+          enter={true}
+          exit={true}
+
+          onEnter={(node) => {
+            console.log('Enter')
+          }}
+
+          onExit={(node) => {
+            console.log('Exit')
+          }}
         >
           {state => (
-            <div style={{
-              background: 'red',
-              height: '100px',
-              transition: 'all 2s ease',
-              opacity: state === 'exited' || state === 'exiting' ? 0 : 1
-            }}>
-              {state}
+            <div className={`square square-${state}`}>
+              {`square square-${state}`}
             </div>
           )}
         </Transition>
