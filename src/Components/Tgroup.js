21b4f84b5ee3c982d8d5d99e106a3aef581f3f87
Peter Xuan' ang <peterxuanang@hotmail.com>
2020-09-10 20:28:19 -0500

    Working with React Transitions for a Group, like a list of elements

--- src/Components/Tgroup.js
+++ src/Components/Tgroup.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+
+// Transitions group is used to render list
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import './../Styles/App.css';
 
 class Slide extends Component {
@@ -8,20 +11,32 @@ class Slide extends Component {
 
   addElements() {
     return this.state.items.map((item, i) => (
-      <div className="item" key={i}>{item}</div>
+      <CSSTransition
+        classNames='item'
+        timeout={5000}
+        key={i}
+        onEntered={node => {
+          node.classList.add('active')
+        }}
+      >
+        <div className="item" key={i}>{item}</div>
+      </CSSTransition>
     ));
   }
 
   generateNumber() {
     //let random = Math.floor(Math.random() * 100) + 1;
     let newArray = [...this.state.items, Math.floor(Math.random() * 100) + 1]
-
+    // console.log(newArray)
     this.setState({
       items: newArray
     })
   }
 
   removeNumber() {
+    // let arr = ['a','b','c','d','e']
+    // let arr2 = arr.slice() - shallow copies the array 'arr' and saves it into another variable
+    // let arr3 = arr.slice(0,-1) - removes last element and copies the rest
     let newArray = this.state.items.slice(0, -1);
     this.setState({
       items: newArray
@@ -31,7 +46,12 @@ class Slide extends Component {
   render() {
     return (
       <div>
-        {this.addElements()}
+        <TransitionGroup
+          component='div'
+          className='list'
+        >
+          {this.addElements()}
+        </TransitionGroup>
 
         <div className="btns">
           <div className="btn-add" onClick={() => this.generateNumber()}>Add Elements</div>
