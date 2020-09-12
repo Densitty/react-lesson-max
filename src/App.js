3ce4bde01e477eefd309110c09ee63cad133497c
Peter Xuan' ang <peterxuanang@hotmail.com>
2020-09-11 20:02:57 -0500

    Controlled state of forms; uses React to handle information processed in forms used state while Uncontrolled state is using refs to handle form data (not that recommended though)

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './Routes'
 import './Styles/App.css';
 
 // COMPONENTS
-import CSStransition from './Components/CSSTransition';
-import Tgroup from './Components/Tgroup';
-import TransitionComp from './Components/Transition';
-import User from './Components/User'
+
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <div className="anim_index">
-          <Link to="/transition">Transition</Link>
-          <Link to="/csstransition">CSStransition</Link>
-          <Link to="/tgroup">Transitions group</Link>
-          <Link to="/user">User</Link>
-        </div>
-        <Route path="/transition" component={TransitionComp}></Route>
-        <Route path="/csstransition" component={CSStransition}></Route>
-        <Route path="/tgroup" component={Tgroup}></Route>
-        <Route path="/user" component={User}></Route>
-      </div>
+      <Routes />
     </BrowserRouter>
   )
 }
--- src/Components/User.js
+++ src/Components/User.js
@@ -1,37 +1,18 @@
-import React from 'react'
-import UserTemplate from './UserTemplate'
+import React, { Component } from 'react';
 
-class User extends React.Component {
+class User extends Component {
+
   state = {
-    name: ['John', 'Kelvin'],
-    lastname: 'Doe',
-    age: 16,
-    hobbies: ['Reading', 'Hockey', 'Javelin', 'Hunting', 'Kayaking'],
-    multilingual: true,
-    languages: ['English', 'Deustch', 'French', 'Spanish', 'Portugues'],
-    friends: ['Jane', 'Sally', 'Rudolf', 'Steve', 'Enchem'],
-    greetings: () => {
-      return (`Welcome to the world of ${this.state.name[0]} ${this.state.lastname}`)
-    },
-    profession: 'student',
-    major: 'Physics',
-    intro() {
-      return `I am a ${this.profession} at University of Western Sydney, studying ${this.major}.`
-    },
-    car: {
-      maker: 'Audi',
-      year: '2007',
-      color: 'Black'
-    }
+
   }
 
   render() {
     return (
       <div>
-        <UserTemplate {...this.state} />
+        User
       </div>
     )
   }
 }
 
-export default User
+export default User;
